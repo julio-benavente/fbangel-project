eabbf03bdad17f54c45426221a4d8eb54b98aabb
Julio Benavente <32945829+julio-benavente@users.noreply.github.com>
2021-06-06 17:22:20 -0500

    Listing serve message changed

--- server.js
+++ server.js
@@ -59,5 +59,5 @@ const port = process.env.PORT || 5000;
 
 // App connection
 app.listen(port, () => {
-  console.log("Server running on port... ");
+  console.log(`Server running on port ${port} `);
 });
