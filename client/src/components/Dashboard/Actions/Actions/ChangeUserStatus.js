ad7fda909d4e632c1a390145fdb5d4ca754990c8
Julio Benavente <julio.benavente.02@gmail.com>
2021-06-18 07:22:30 -0500

    Translations: Creation action translation modifed

--- client/src/components/Dashboard/Actions/Actions/ChangeUserStatus.js
+++ client/src/components/Dashboard/Actions/Actions/ChangeUserStatus.js
@@ -68,9 +68,7 @@ const ChangeUserStatus = ({ setItemsSelected, itemsSelected, setAction }) => {
     );
 
     // remove to users selected list
-    setUsersSelected(
-      usersSelected.filter((userElem) => userElem._id !== user._id)
-    );
+    setUsersSelected(usersSelected.filter((userElem) => userElem._id !== user._id));
   };
 
   const [usersSelectedIsOpen, setUsersSelectedIsOpen] = useState(false);
@@ -83,6 +81,7 @@ const ChangeUserStatus = ({ setItemsSelected, itemsSelected, setAction }) => {
       label: t("actions.change_user_status.referral_code"),
       value: "referralCode",
     },
+    { label: t("actions.change_user_status.email"), value: "email" },
     { label: t("actions.change_user_status.first_name"), value: "firstName" },
     { label: t("actions.change_user_status.last_name"), value: "lastName" },
   ];
@@ -102,34 +101,19 @@ const ChangeUserStatus = ({ setItemsSelected, itemsSelected, setAction }) => {
           <>
             <Filter className="filter">
               <p>{t("actions.change_user_status.filter")} : </p>
-              <input
-                className="filterData"
-                type="text"
-                onChange={(e) => setFilterData(e.target.value)}
-              />
+              <input className="filterData" type="text" onChange={(e) => setFilterData(e.target.value)} />
               <div className="selectWrapper">
-                <Select
-                  options={filterOption}
-                  onChange={({ value }) => setFilterAttribute(value)}
-                />
+                <Select options={filterOption} onChange={({ value }) => setFilterAttribute(value)} />
               </div>
             </Filter>
             <div className="thead">
               <div className="tr" style={{ ...tableWidth }}>
                 <div className="th select"></div>
-                <div className="th name">
-                  {t("actions.change_user_status.first_name")}
-                </div>
-                <div className="th email">
-                  {t("actions.change_user_status.last_name")}
-                </div>
-                <div className={`td status`}>
-                  {t("actions.change_user_status.status")}
-                </div>
+                <div className="th name">{t("actions.change_user_status.first_name")}</div>
+                <div className="th email">{t("actions.change_user_status.email")}</div>
+                <div className={`td status`}>{t("actions.change_user_status.status")}</div>
 
-                <div className="th referralCode">
-                  {t("actions.change_user_status.referral_code")}
-                </div>
+                <div className="th referralCode">{t("actions.change_user_status.referral_code")}</div>
               </div>
             </div>
             <div className="tbody">
@@ -139,40 +123,19 @@ const ChangeUserStatus = ({ setItemsSelected, itemsSelected, setAction }) => {
                     return true;
                   }
 
-                  return user[filterAttribute]
-                    .toLowerCase()
-                    .includes(filterData.toLowerCase());
+                  return user[filterAttribute].toLowerCase().includes(filterData.toLowerCase());
                 })
                 .map((user, index) => {
-                  const {
-                    firstName,
-                    lastName,
-                    email,
-                    referralCode,
-                    select,
-                    status,
-                  } = user;
+                  const { firstName, lastName, email, referralCode, select, status } = user;
                   return (
                     <div className="tr" key={index} style={{ ...tableWidth }}>
                       <div className="td select">
-                        {!select && (
-                          <div
-                            className="box-select"
-                            onClick={() => addToSelection(user)}
-                          ></div>
-                        )}
-                        {select && (
-                          <div
-                            className="box-selected"
-                            onClick={() => removeFromSelection(user)}
-                          ></div>
-                        )}
+                        {!select && <div className="box-select" onClick={() => addToSelection(user)}></div>}
+                        {select && <div className="box-selected" onClick={() => removeFromSelection(user)}></div>}
                       </div>
                       <div className="td name">{`${firstName} ${lastName}`}</div>
                       <div className="td email">{email}</div>
-                      <div
-                        className={`td status ${status}`}
-                      >{`${status[0].toUpperCase()}${status.slice(1)}`}</div>
+                      <div className={`td status ${status}`}>{`${status[0].toUpperCase()}${status.slice(1)}`}</div>
                       <div className="td referralCode">{referralCode}</div>{" "}
                     </div>
                   );
@@ -227,9 +190,7 @@ const UserSelectedTable = (props) => {
   const onSubmit = async (data) => {
     const informationToSend = Object.keys(data).map((key) => ({
       item: key,
-      action: `Status changed to ${data[key].status[0].toUpperCase()}${data[
-        key
-      ].status.slice(1)}`,
+      action: `Status changed to ${data[key].status[0].toUpperCase()}${data[key].status.slice(1)}`,
       modification: {
         status: data[key].status,
         statusObservation: data[key].statusObservation || "",
@@ -244,10 +205,7 @@ const UserSelectedTable = (props) => {
     };
 
     try {
-      const response = await axios.post(
-        "/api/actions/change-user-status",
-        action
-      );
+      const response = await axios.post("/api/actions/change-user-status", action);
 
       // clean selections
       setAction("");
@@ -260,57 +218,32 @@ const UserSelectedTable = (props) => {
     <UsersSelected>
       <form onSubmit={handleSubmit(onSubmit)}>
         <UsersSelectedSummary>
-          <button
-            type="button"
-            onClick={() => setUsersSelectedIsOpen(!usersSelectedIsOpen)}
-          >
+          <button type="button" onClick={() => setUsersSelectedIsOpen(!usersSelectedIsOpen)}>
             {!usersSelectedIsOpen ? "Ver selecionados" : "Ocultar selecionados"}
           </button>
           {usersSelectedIsOpen && (
             <p>
-              {t("actions.change_user_status.users_selected")} :{" "}
-              {usersSelected.length}
+              {t("actions.change_user_status.users_selected")} : {usersSelected.length}
             </p>
           )}
 
-          {usersSelectedIsOpen && (
-            <button type="submit">
-              {t("actions.change_user_status.send")}
-            </button>
-          )}
+          {usersSelectedIsOpen && <button type="submit">{t("actions.change_user_status.send")}</button>}
         </UsersSelectedSummary>
         {usersSelectedIsOpen && (
           <>
             <div className="thead">
               <div className="tr" style={{ ...tableWidth }}>
                 <div className="th select"></div>
-                <div className="th name">
-                  {t("actions.change_user_status.name")}
-                </div>
-                <div className="th email">
-                  {t("actions.change_user_status.email")}
-                </div>
-                <div className="th referralCode">
-                  {t("actions.change_user_status.referral_code")}
-                </div>
-                <div className="th status">
-                  {t("actions.change_user_status.status")}
-                </div>
-                <div className="th statusObservation">
-                  {t("actions.change_user_status.observation")}
-                </div>
+                <div className="th name">{t("actions.change_user_status.name")}</div>
+                <div className="th email">{t("actions.change_user_status.email")}</div>
+                <div className="th referralCode">{t("actions.change_user_status.referral_code")}</div>
+                <div className="th status">{t("actions.change_user_status.status")}</div>
+                <div className="th statusObservation">{t("actions.change_user_status.observation")}</div>
               </div>
             </div>
             <div className="tbody">
               {usersSelected.map((user, index) => {
-                const {
-                  firstName,
-                  lastName,
-                  email,
-                  referralCode,
-                  select,
-                  _id,
-                } = user;
+                const { firstName, lastName, email, referralCode, select, _id } = user;
                 return (
                   <div className="tr" key={index} style={{ ...tableWidth }}>
                     <div className="td select">
@@ -332,12 +265,7 @@ const UserSelectedTable = (props) => {
                         render={({ field: { onChange } }) => (
                           <div className="inputWrapper">
                             <Select
-                              className={`select ${
-                                errors &&
-                                errors[_id] &&
-                                errors[_id].status &&
-                                "error"
-                              } `}
+                              className={`select ${errors && errors[_id] && errors[_id].status && "error"} `}
                               options={statusOptions}
                               onChange={({ value }) => {
                                 onChange(value);
@@ -349,10 +277,7 @@ const UserSelectedTable = (props) => {
                     </div>
                     <div className="td statusObservation">
                       <div className="inputWrapper">
-                        <input
-                          type="text"
-                          {...register(`${_id}.statusObservation`)}
-                        />
+                        <input type="text" {...register(`${_id}.statusObservation`)} />
                       </div>
                     </div>
                   </div>
