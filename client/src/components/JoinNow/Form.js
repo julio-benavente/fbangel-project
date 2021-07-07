b8f249631c3a7a3c820aedcd47e51dae17c4fc85
Julio Benavente <julio.benavente.02@gmail.com>
2021-07-07 12:38:16 -0500

    Default values for the form available in development

--- client/src/components/JoinNow/Form.js
+++ client/src/components/JoinNow/Form.js
@@ -82,7 +82,7 @@ const Form = () => {
       stepFour: {
         referral: token,
       },
-      ...defaultValues,
+      defaultValues: process.env.NODE_ENV === "development" ? defaultValues : null,
     },
   });
 
